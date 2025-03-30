58d16ce69a5b9f8bebcd62c73a92adaed2ca4be4
YaSanyaBeats <alexandr191261@yandex.ru>
2025-03-30 17:49:03 +0700

    Fix Alexey

--- src/js/app.js
+++ src/js/app.js
@@ -1,70 +1,17 @@
 const isMobile = document.documentElement.clientWidth < 768;
 const isTablet = document.documentElement.clientWidth < 1140;
-const scrollParam = 50;
-function isWebp() {
-    // Проверка поддержки webp
-    const testWebp = (callback) => {
-        const webP = new Image();
 
-        webP.onload = webP.onerror = () => callback(webP.height === 2);
-        webP.src =
-        'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-    };
+window.accordeons = {};
 
-    // Добавление класса _webp или _no-webp для HTML
-    testWebp((support) => {
-        const className = support ? 'webp' : 'no-webp';
-        document.querySelector('html').classList.add(className);
-        console.log(support ? 'webp поддерживается' : 'webp не поддерживается');
+function initAccordeons() {
+    window.accordeons.footer = UIkit.accordion($('.footer__block-menu'), {
+       content: '> .footer__menu-items',
+       toggle: '> .footer__menu-title'
     });
-}
-
-isWebp();
-
-function initCopyCode() {
-    let codes = document.querySelectorAll('.code__body');
-    codes.forEach((elem) => {
-        elem.addEventListener('click', (event) => {
-            UIkit.notification.closeAll()
-            navigator.clipboard.writeText(elem.innerText).then(function() {
-                UIkit.notification({
-                    message: 'Код успешно скопирован',
-                    status: 'primary',
-                    pos: 'bottom-center',
-                    group: false,
-                    timeout: 5000
-                });
-            }, function(err) {
-                console.error('Произошла ошибка при копировании текста: ', err);
-            });
-        })
-    })
-}
-
-function checkScroll() {
-    if (window.scrollY > scrollParam) {
-        return true;
-    }
-    return false;
-}
-function adaptiveScrollHeader() {
-    let header = document.querySelector("header");
-    function check()
-    {
-        if (checkScroll()) {
-            header.classList.add("active");
-        }
-        if (!checkScroll()) {
-            header.classList.remove("active");
-        }
-    }
-    document.addEventListener('scroll', (event) => {
-        check();
-    });
-    check();
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    initCopyCode();
-    adaptiveScrollHeader();
+    if(isMobile) {
+        initAccordeons();
+    }
 })
