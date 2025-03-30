26a0756e86f9ce9d274606721a4b131a3cef94ae
YaSanyaBeats <alexandr191261@yandex.ru>
2025-03-30 23:40:24 +0700

    Adaprive hero

--- src/js/app.js
+++ src/js/app.js
@@ -10,8 +10,52 @@ function initAccordeons() {
     });
 }
 
+function initScrollHandler() {
+    updateScrollHandler();
+    $(window).on('scroll', updateScrollHandler);
+}
+
+function updateScrollHandler() {
+    let currentScroll = $(this).scrollTop();
+    if(currentScroll > 20) {
+        $('body').addClass('scroll');
+        hideFixedMenu();
+    }
+    else {
+        $('body').removeClass('scroll');
+        showFixedMenu();
+    }
+}
+
+function initFixedMenu() {
+    let list = $('.menu__list');
+    let button = $('.menu__button');
+    button.on('click', function(event) {
+        event.preventDefault();
+        list.toggleClass('menu__list_active');
+        button.toggleClass('menu__button_active');
+        button.blur();
+    })
+}
+
+function showFixedMenu() {
+    let list = $('.menu__list');
+    let button = $('.menu__button');
+    list.addClass('menu__list_active');
+    button.addClass('menu__button_active');
+}
+
+function hideFixedMenu() {
+    let list = $('.menu__list');
+    let button = $('.menu__button');
+    list.removeClass('menu__list_active');
+    button.removeClass('menu__button_active');
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     if(isMobile) {
         initAccordeons();
     }
+    initScrollHandler();
+    initFixedMenu();
 })
